32f0b15291503b9d0f938bfcf060ce8cf957e011
Alberto Zurini <alberto.zurini@gmail.com>
2025-04-05 05:59:16 +0300

    feat: added request network connection

--- src/app/client/requests/new/page.tsx
+++ src/app/client/requests/new/page.tsx
@@ -1,10 +1,15 @@
-import NewRequestForm from '@/components/NewRequestForm';
+import NewRequestForm from "@/components/NewRequestForm";
 
 export default function NewRequestPage() {
-    return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold mb-6">Create New Request</h1>
-            <NewRequestForm />
-        </div>
-    );
-} 
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Create New Request</h1>
+      <NewRequestForm />
+      <iframe
+        src="https://invoicing-template-fwjmya1ct-albertos-projects-434a5907.vercel.app/create-invoice"
+        width="100%"
+        height="100vh"
+      ></iframe>
+    </div>
+  );
+}
